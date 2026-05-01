b5f37b47068e5dcc2bfbc0c14bbe150fae523651
EmilTsaturyan <emiltsaturyan60@gmail.com>
2026-05-01 19:16:42 +0400

    Fix error

--- agent/base.py
+++ agent/base.py
@@ -1,13 +1,15 @@
-from langchain_core.prompts import ChatPromptTemplate
 
 
 class BaseAgent:
     def __init__(self, llm, system_prompt: str):
         self.llm = llm
-        self.prompt = ChatPromptTemplate.from_messages([
-            ("system", system_prompt),
-            ("human", "{input}")
-        ])
+        self.prompt = {
+            "messages": [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": ""}
+            ]
+        }
 
     def run(self, input_text: str):
-        return self.llm.invoke(self.prompt.format(input=input_text)).get("messages")[-1].text
+        self.prompt["messages"][1]["content"] = input_text
+        return self.llm.invoke(self.prompt).get("messages")[-1].text
